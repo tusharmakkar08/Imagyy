b712a24535aa292406f030649975c9a865740e61
Tushar Makkar <tusharmakkar08@gmail.com>
2014-12-25 02:13:31 +0530

    Modular code

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-import urllib, json , webbrowser
-url = "http://graph.facebook.com/tanya.jain.129"
-response = urllib.urlopen(url);
-data = json.loads(response.read())
-print data['id']
-new_url = "https://www.facebook.com/search/"+data['id']+"/photos-of"
-webbrowser.open_new_tab(new_url)
+import json, urllib, webbrowser
+
+def get_profile_id(username):
+    """
+    Input : Username
+    Output : Profile Id 
+    """
+    url = "http://graph.facebook.com/" + username
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+    return data['id']
+    
+def open_image_page(profile_id):
+    """
+    Input : Profile Id 
+    Output : Opens a new tab with graph search results
+    """
+    new_url = "https://www.facebook.com/search/"+profile_id+"/photos-of"
+    webbrowser.open_new_tab(new_url)
+
+def main():
+    username = raw_input("Enter the Username : ")
+    user_id = get_profile_id(username)
+    open_image_page(user_id)
+    print ""
+    return 0
+
+if __name__ == '__main__':
+	main()
