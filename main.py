1a87760dcc5f3f0700ae9ee486f94cc768419ddc
Tushar Makkar <tushar@codenation.co.in>
2016-05-17 18:28:13 +0530

    Issue-6: Added requirements.txt and .gitignore

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+__author__ = 'tusharmakkar08'
+
+import json
+import urllib
+import webbrowser
+
 from bs4 import BeautifulSoup
-import json, requests, sys, urllib, webbrowser
+import requests
+
 
 def get_profile_id_fbv1(username):
     """
@@ -10,37 +17,42 @@ def get_profile_id_fbv1(username):
     response = urllib.urlopen(url)
     data = json.loads(response.read())
     return data['id']
-    
+
+
 def get_profile_id_fbv2(username):
     """
     Input : Username
     Output : Profile Id HTML 
     """
-    requestData = requests.post("http://findmyfbid.com/", data={'url':username})
-    return requestData.text
+    request_data = requests.post("http://findmyfbid.com/", data={'url': username})
+    return request_data.text
+
 
 def get_user_id(html_string):
     """
     Input : HTML String from POST
     Output : Profile Id  
     """
-    return BeautifulSoup(html_string,"html.parser").code.string
+    return BeautifulSoup(html_string, "html.parser").code.string
+
 
 def open_image_page(profile_id):
     """
     Input : Profile Id 
     Output : Opens a new tab with graph search results
     """
-    new_url = "https://www.facebook.com/search/"+profile_id+"/photos-of"
+    new_url = "https://www.facebook.com/search/" + profile_id + "/photos-of"
     webbrowser.open_new_tab(new_url)
+
 
 def open_profile_pic(profile_id):
     """
     Input : Profile Id 
     Output : Opens a new tab with profile picture of the username
     """
-    new_url = "https://graph.facebook.com/"+profile_id+"/picture?width=800"
+    new_url = "https://graph.facebook.com/" + profile_id + "/picture?width=800"
     webbrowser.open_new_tab(new_url)
+
 
 def main():
     username = raw_input("Enter the Username : ")
@@ -50,6 +62,7 @@ def main():
     open_profile_pic(user_id)
     print ""
     return 0
+
 
 if __name__ == '__main__':
     main()
